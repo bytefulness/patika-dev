3854a2e45107c7603102e50ac8de0dce4b1e90b9
eakyel <e.akyel@outlook.com>
2022-01-09 16:30:26 +0300

    Create component

---
+++ react/tutorial/app/src/App.js
@@ -0,0 +1,8 @@
+// Import Components
+import Header from "./components/Header";
+
+function App() {
+  return <Header />;
+}
+
+export default App;
---
+++ react/tutorial/app/src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h1>Hello, it's header component.</h1>
+    </header>
+  );
+}
+
+export default Header;
