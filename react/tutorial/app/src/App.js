c26fdc00a3632654b1c0ca837208cb727b4f5f6f
eakyel <e.akyel@outlook.com>
2022-01-09 16:51:35 +0300

    Render variables, object in components

--- react/tutorial/app/src/App.js
+++ react/tutorial/app/src/App.js
@@ -1,8 +1,18 @@
 // Import Components
 import Header from "./components/Header";
 
+const greeting = (name) => {
+  return `Hello ${name}`;
+};
+
 function App() {
-  return <Header />;
+  return (
+    // Use fragment to create parent without creating element.
+    <>
+      <Header />
+      <h2>Greeting: {greeting("Erhan")}</h2>
+    </>
+  );
 }
 
 export default App;
