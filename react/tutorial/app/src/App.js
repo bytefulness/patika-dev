bc4f07fd410d6c1e9f84c37d6b3463a42f299595
eakyel <e.akyel@outlook.com>
2022-01-10 14:33:54 +0300

    Tutorial: List items with key prop

--- react/tutorial/app/src/App.js
+++ react/tutorial/app/src/App.js
@@ -1,11 +1,19 @@
 // Import Components
 import User from "./components/User";
 
+const friends = ["Dilara", "Gamze", "Mehmet", "Cansu"];
+
 function App() {
   return (
     // Use fragment to create parent without creating element.
     <>
-      <User name="Erhan" age={24} field="Front-end" devTeam={true} />
+      <User
+        name="Erhan"
+        age={24}
+        field="Front-end"
+        friends={friends}
+        devTeam={true}
+      />
     </>
   );
 }
--- react/tutorial/app/src/components/User.js
+++ react/tutorial/app/src/components/User.js
@@ -1,10 +1,18 @@
-function User({ name, age, field, devTeam }) {
+function User({ name, age, field, friends, devTeam }) {
   return (
-    <h1>
-      {devTeam
-        ? `Hello ${name} (${age}) you're in devTeam your field is ${field}`
-        : `Sorry ${name} you have no access`}
-    </h1>
+    <>
+      <h1>
+        {devTeam
+          ? `Hello ${name} (${age}) you're in devTeam your field is ${field}`
+          : `Sorry ${name} you have no access`}
+      </h1>
+
+      <ul>
+        {friends.map((friend, id) => (
+          <li key={id}>{friend}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
